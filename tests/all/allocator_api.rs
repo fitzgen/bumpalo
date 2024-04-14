a639ea303516e32c04ce54dfc47f886118d9daa0
Nick Fitzgerald <fitzgen@gmail.com>
2024-04-14 12:18:56 -0700

    Fix warning when testing without the `allocator_api` cargo feature

--- tests/all/allocator_api.rs
+++ tests/all/allocator_api.rs
@@ -1,11 +1,40 @@
 #![cfg(feature = "allocator_api")]
 
 use crate::quickcheck;
-use crate::quickcheck::arbitrary_layout;
 use bumpalo::Bump;
 use std::alloc::{AllocError, Allocator, Layout};
 use std::ptr::NonNull;
 use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
+
+/// Map an arbitrary `x` to a power of 2 that is less than or equal to `max`,
+/// but with as little bias as possible (eg rounding `min(x, max)` to the
+/// nearest power of 2 is unacceptable because it would majorly bias `max` for
+/// small values of `max`).
+fn clamp_to_pow2_in_range(x: usize, max: usize) -> usize {
+    let log_x = max.ilog2() as usize;
+    if log_x == 0 {
+        return 1;
+    }
+    let divisor = usize::MAX / log_x;
+    let y = 1_usize << (x / divisor);
+    assert!(y.is_power_of_two(), "{y} is not a power of two");
+    assert!(y <= max, "{y} is larger than {max}");
+    y
+}
+
+/// Helper to turn a pair of arbitrary `usize`s into a valid `Layout` of
+/// reasonable size for use with quickchecks.
+pub fn arbitrary_layout(size: usize, align: usize) -> Layout {
+    const MAX_ALIGN: usize = 64;
+    const MAX_SIZE: usize = 1024;
+
+    let align = clamp_to_pow2_in_range(align, MAX_ALIGN);
+
+    let size = size % (MAX_SIZE + 1);
+    let size = size.next_multiple_of(align);
+
+    Layout::from_size_align(size, align).unwrap()
+}
 
 #[derive(Debug)]
 struct AllocatorDebug {
